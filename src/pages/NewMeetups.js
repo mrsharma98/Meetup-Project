2aaadda3ebaddc073d93643d396c84268a394f59
Manish Sharma <manish.sharma@metadesignsoftware.com>
2021-05-15 21:00:51 +0530

    added firebase api for storing data

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,32 @@
+import { useHistory } from 'react-router-dom'
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupsPage() {
+
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+
+    fetch(
+      'https://react-meetup-b15dc-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }       
+    ).then(() => {
+      history.replace('/')
+    })
+
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   )
 }
