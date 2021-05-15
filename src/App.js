f37ba06c8c505e87f61c7635d55ef02c2d9100cc
Manish Sharma <manish.sharma@metadesignsoftware.com>
2021-05-15 12:58:05 +0530

    added layout and meetup components

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups'
 import NewMeetupsPage from './pages/NewMeetups'
 import FavoritesPage from './pages/Favourites'
-import MainNavigation from './components/layout/MainNavigation'
+import Layout from './components/layout/Layout'
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path='/' exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path='/new-meetup'>
-          <NewMeetupsPage />
-        </Route>
-        <Route path='/favorites'>
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path='/' exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path='/new-meetup'>
+            <NewMeetupsPage />
+          </Route>
+          <Route path='/favorites'>
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
+
     </div>
   );
 }
